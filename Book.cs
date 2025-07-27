ded7ddd3f90b1fde66a72d022c5f1ba6c121bd48
Bilolbek <124268054+delolipop777@users.noreply.github.com>
2025-07-27 11:59:55 +0500

    first

---
+++ Book.cs
@@ -0,0 +1,35 @@
+using System.Security.Cryptography;
+
+class Book
+{
+    string _name;
+    string _author;
+    double _price;
+
+    public Book(string name, string Author, double price)
+    {
+        _name = name;
+        _price = price;
+    }
+
+    public string Name => _name;
+    public double Price { get; set; }
+    public string Author => _author;
+
+
+    public void PrintInfo()
+    {
+        Console.WriteLine($"Name: {_name}\nAuthor: {_author}\nPrice: {_price}");
+    }
+
+    public void ApplyDiscount(double percent)
+    {
+        _price = _price * (1 - percent / 100);
+        Console.WriteLine($"Price after discount: {_price}");
+    }
+
+    public bool IsExpensive()
+    {
+        return _price > 10;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Book myBook = new Book("Muqaddima", "Ibn Xaldun", 6.5);
+
+//narxlar qimmatladi
+
+myBook.Price = 7.5;
+
+myBook.PrintInfo();
+
+myBook.ApplyDiscount(5);
+
+myBook.IsExpensive();
